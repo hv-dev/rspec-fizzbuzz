fd23cb2046d37e2139f276627b65b940ad3af401
Harry Verrall <harryv@chillibean.com>
2022-09-09 14:41:52 +0100

    First implementation
    First implementation of a solution to the fizzbuzz challenge.
    
    Awaiting rspec testing

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+    fizz = num % 3 == 0
+    buzz = num % 5 == 0
+
+    if fizz && buzz
+        return "FizzBuzz"
+    elsif fizz
+        return "Fizz"
+    elsif buzz
+        return "Buzz"
+    else
+        return
+    end
+end
